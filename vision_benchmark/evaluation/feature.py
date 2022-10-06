ac819b432b04faf60e518bb77e4971e2de0e39db
Ximeng Sun <sunxm@fb.com>
2022-10-06 14:06:00 -0400

    change run_multi

--- vision_benchmark/evaluation/feature.py
+++ vision_benchmark/evaluation/feature.py
@@ -237,6 +237,8 @@ def load_custom_zeroshot_model(config):
     for k, v in state_dict.items():
         new_k = k.replace('lang_encoder', 'text').replace('lang_projection', 'text_projection').replace('image_encoder', 'visual').replace('image_projection', 'vision_projection')
         new_state_dict[new_k] = v
+    print('new_state_dict keys: ', new__state_dict.keys())
+    print('model keys: ', model.state_dict()..keys())
     msg = model.load_state_dict(new_state_dict, strict=False)
     print(f'loading checkpoint msg: {msg}')
     return model
