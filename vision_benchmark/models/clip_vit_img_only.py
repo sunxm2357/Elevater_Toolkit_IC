f028a571c24380d1c99161c8fb8ff115437d4852
Ximeng Sun <sunxm@fb.com>
2022-10-20 13:50:05 -0400

    add vit distill eval

--- vision_benchmark/models/clip_vit_img_only.py
+++ vision_benchmark/models/clip_vit_img_only.py
@@ -117,12 +117,12 @@ class Transformer(nn.Module):
 
     def _init_weights(self, m):
         if isinstance(m, (nn.Linear, nn.Conv2d)):
-            if is_main_process():
-                logger.info("=> init weight of Linear/Conv2d from trunc norm")
+            # if is_main_process():
+            #     logger.info("=> init weight of Linear/Conv2d from trunc norm")
             trunc_normal_(m.weight, std=0.02)
             if m.bias is not None:
-                if is_main_process():
-                    logger.info("=> init bias of Linear/Conv2d to zeros")
+                # if is_main_process():
+                #     logger.info("=> init bias of Linear/Conv2d to zeros")
                 nn.init.constant_(m.bias, 0)
         elif isinstance(m, (nn.LayerNorm, nn.BatchNorm2d)):
             nn.init.constant_(m.bias, 0)
