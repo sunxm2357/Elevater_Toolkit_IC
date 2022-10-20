668624a432bab889810a5a7e92d9c2171c3272ed
Ximeng Sun <sunxm@fb.com>
2022-10-20 13:44:42 -0400

    add vit distill eval

--- vision_benchmark/models/__init__.py
+++ vision_benchmark/models/__init__.py
@@ -3,8 +3,10 @@ from . import clip_example
 from . import cls_swin
 from . import clip_swin
 from . import clip_swin_img_only
+from . import clip_vit_img_only
 from . import mae
 from . import mocov3
 from . import declip
 
-__all__ = ['cls_example', 'clip_example', 'cls_swin', 'clip_swin', 'mae', 'mocov3', 'declip', 'clip_swin_img_only']
+__all__ = ['cls_example', 'clip_example', 'cls_swin', 'clip_swin', 'mae', 'mocov3', 'declip', 'clip_swin_img_only',
+           'clip_vit_img_only']
