53c64d75ce07fac5c97c374015e893b2710d4fb3
Ximeng Sun <sunxm@fb.com>
2022-11-26 12:38:03 -0500

    add more experiments

--- submit_script_lp/swin_in21k_yfcc14m/food101.sh
+++ submit_script_lp/swin_in21k_yfcc14m/food101.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=in21k_yfcc14m_cc15m_pseudo_food101_fullset_lp
+#SBATCH --job-name=swin_tiny_food101_fullset_lp
 #SBATCH --partition=learnai4rl
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=12
@@ -10,9 +10,9 @@
 #SBATCH --output=log_food101_fullset_lp.o.log
 
 OUTPUT_DIR=/checkpoints/sunxm/output/evaluations
-MODEL_CFG=clip_vit_distill
+MODEL_CFG=clip_swin
 MODE=linear_probe
-CKPT=/checkpoints/sunxm/output/EfficientVLPTaskNew/image_text_unpaired_v2/in21k-cc15m-yfcc14m-512-pseudo_0.3-lr0.0008-wd0.05-bs12288-clip0.0-g24/clip_ViT-B32-in21k-cc15m-yfcc14m-512-pseudo_0.3.yaml_conf~/run_1/324800/default/model_state_dict.pt
+CKPT=/fsx/sunxm/models/in21k_yfcc14m.pth
 
 cd /fsx/sunxm/code/Elevater_Toolkit_IC
 
