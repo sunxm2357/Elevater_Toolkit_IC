13ee97f0dd5d240856e681a98b1665e0baf09285
Ximeng Sun <sunxm@rfc1918.address.not.used.bu.edu>
2023-01-22 17:09:29 -0500

    add a new script for scc lp

--- robustness_old_script.sh
+++ robustness_old_script.sh
@@ -3,6 +3,8 @@
 cd vision_benchmark
 output_dir=/research/sunxm/output/evaluations/
 
+CUDA_VISIBLE_DEVICES=$2 python commands/zeroshot.py --ds resources/datasets/imagenet-1k.yaml  --model resources/model/vitb32_student.yaml  MODEL.PRETRAINED $1 DATASET.ROOT $output_dir/datasets OUTPUT_DIR $output_dir/$model_cfg/log
+
 CUDA_VISIBLE_DEVICES=$2 python commands/zeroshot.py --ds resources/datasets/imagenetv2.yaml  --model resources/model/vitb32_student.yaml  MODEL.PRETRAINED $1 DATASET.ROOT $output_dir/datasets OUTPUT_DIR $output_dir/$model_cfg/log
 
 CUDA_VISIBLE_DEVICES=$2 python commands/zeroshot.py --ds resources/datasets/imagenet-r.yaml  --model resources/model/vitb32_student.yaml  MODEL.PRETRAINED $1 DATASET.ROOT $output_dir/datasets OUTPUT_DIR $output_dir/$model_cfg/log
